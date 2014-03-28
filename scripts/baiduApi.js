dcd9c5620ed35a8ca67d72f9f97acee3b18e3292
vincent zhang <vincent@louding.com>
2014-03-28 11:45:11 +0800

    baiduapi

--- scripts/baiduApi.js
+++ scripts/baiduApi.js
@@ -1,4 +1,5 @@
 var http_sync = require('./http-sync/http-sync');
+var urlencode = require('urlencode');
 
 var values = ["徐家汇", "人民广场", "中山公园", "宜山路", "漕河泾开发区", "张江高科", "佘山", "泗泾", "杨高中路", "国权路"];
 
@@ -7,18 +8,15 @@ for (var i=0; i<values.length; i++) {
   var req = http_sync.request({
     protocol: 'http',
     host: 'api.map.baidu.com',
-    // path: '/geocoder/v2/' + '?address=%E5%BE%90%E5%AE%B6%E6%B1%87&output=json&ak=uPcvBuG2dlnPlP2wKPfr4Tk2&callback=showLocation'
-    path: '/geocoder/v2/' + '?address='+values[i].toString("UTF8")+'&output=json&ak=uPcvBuG2dlnPlP2wKPfr4Tk2&callback=showLocation'
+    path: '/geocoder/v2/' + '?address='+urlencode(values[i])+'&output=json&ak=uPcvBuG2dlnPlP2wKPfr4Tk2&callback=showLocation'
   });
 
-  console.log(values[i].toString('UTF8'));
   var res = req.end();
-  console.log("it comes >>>>>>>>>>>>>" + i);
   console.log(res.body.toString());
 }
 
 
 var http = require('http');
 console.log("<<<<<<<<<<<<<<<<<<<<<<<<");
-http.request('http://api.map.baidu.com/geocoder/v2/', function(err, res){
+http.request('http://api.map.baidu.com/geocoder/v2/', function(err, res){ 
     });
