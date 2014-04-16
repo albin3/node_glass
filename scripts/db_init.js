35e0da96085bfb1c580c50f36f0fd1ebadf25f53
Albin <albin@louding.com>
2014-04-16 11:14:27 +0800

    在数据库中导入中国的地域信息

--- scripts/db_init.js
+++ scripts/db_init.js
@@ -1,15 +1,19 @@
 // Copyright © 2014 Albin. All Rights Reserved.
 // 配置系统环境，在mongodb中写入初始化信息
 // 包括设置用户名唯一索引等...
+//     将中国地省市区导入到mongodb...
 
 var config = require('../config');
     MongoClient = require('mongodb'),
+    mongojs = require('mongojs'),
+    db = mongojs(config.dbinfo.dbname),
+    dbregional = db.collection('regional'),
+    xlsx = require('node-xlsx'),
     password_hash = require('password-hash');
 
 // 建立后台用户的唯一索引
 MongoClient.connect(config.dbinfo.dbpath, function (err, db) {
       
-
       // 清空数据库
       db.dropDatabase(function () {
         var dbaccount = db.collection('account');
@@ -141,3 +145,41 @@ MongoClient.connect("mongodb://" + config.dbinfo.dbhost + ":" + config.dbinfo.db
           db.close();
       });
 });
+
+// regional 唯一index索引
+MongoClient.connect("mongodb://" + config.dbinfo.dbhost + ":" + config.dbinfo.dbport 
+    + "/" + config.dbinfo.dbname + "?w=1", function (err, db) {
+      var dbaccount = db.collection('regional');
+
+      // 建索引
+      dbaccount.ensureIndex({ index: 1 },
+        { unique: true, dropDups: true, sparse: true },
+        function (err) {
+          if (err) { 
+            console.log(err.message); 
+          } else { 
+            console.log("regional create workerid as unique index success"); 
+          }
+          db.close();
+      });
+});
+// 将中国地省市县数据导入到mongodb中
+var load_regional = function() {
+  var objs = xlsx.parse(__dirname+"/regional.xlsx").worksheets[0].data;
+  for (var i=0; i<objs.length; i++) {
+    var obj = objs[i];
+    if (typeof obj[4].value !== "number" || obj[4].value !== 2) {
+      console.log(obj[4].value);
+      console.log("it's string or it's not 2");
+      continue;
+    }
+    var data = {};
+    data.index = obj[0].value;
+    data.prov  = obj[1].value;
+    data.city  = obj[2].value;
+    data.county= obj[3].value;
+    dbregional.insert(data, function(err) {
+    });
+  }
+};
+load_regional();
