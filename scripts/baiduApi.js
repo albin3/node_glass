cc6ef12e059c9f0696d59c4dd8cb7e884828adb0
Albin <albin@louding.com>
2014-04-24 16:08:06 +0800

    上传店铺信息

--- scripts/baiduApi.js
+++ scripts/baiduApi.js
@@ -1,5 +1,29 @@
-var http_sync = require('./http-sync/http-sync');
-var urlencode = require('urlencode');
+var http_sync = require('./http-sync');
+var urlencode = require('urlencode'),
+    MongoClient = require('mongodb'),
+    mongojs = require('mongojs'),
+    db = mongojs(config.dbinfo.dbname),
+    dbregional = db.collection('regional'),
+    xlsx = require('node-xlsx'),
+    password_hash = require('password-hash');
+
+// 建立后台用户的唯一索引
+MongoClient.connect(config.dbinfo.dbpath, function (err, db) {
+      // 清空数据库
+        var dbaccount = db.collection('stores');
+        // 建索引
+        dbaccount.ensureIndex({ gps: "2d" },
+          {},
+          function (err) {
+            if (err) { 
+              console.log(err.message); 
+            } else { 
+              console.log("create 2d index success"); 
+            }
+            db.close();
+          }
+          );
+});
 
 var values = ["徐家汇", "人民广场", "中山公园", "宜山路", "漕河泾开发区", "张江高科", "佘山", "泗泾", "杨高中路", "国权路"];
 
